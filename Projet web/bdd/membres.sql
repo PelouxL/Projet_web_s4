0305222ff0d573d082ac21bcf968da1827d1d4a4
Rabbras <149598576+Rabbras@users.noreply.github.com>
2024-04-24 12:28:26 +0200

    membres.sql

--- Projet web/bdd/membres.sql
+++ Projet web/bdd/membres.sql
@@ -1,9 +1,49 @@
-
+/*
 CREATE TABLE membres(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     pseudo TEXT,
     mdp TEXT,
     statut INTEGER
+); */
+
+
+CREATE TABLE Utilisateurs (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nom TEXT,
+    email TEXT UNIQUE,
+    date_inscription DATE
 );
+
+CREATE TABLE Amis (
+    id INTEGER PRIMARY KEY,
+    utilisateur_id INTEGER,
+    ami_id INTEGER,
+    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id),
+    FOREIGN KEY (ami_id) REFERENCES Utilisateurs(id)
+);
+
+CREATE TABLE Messages (
+    id INTEGER PRIMARY KEY,
+    utilisateur_id INTEGER,
+    contenu TEXT,
+    date_publication DATETIME,
+    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)
+);
+
+CREATE TABLE Preferences (
+    id INTEGER PRIMARY KEY,
+    utilisateur_id INTEGER,
+    genre TEXT,
+    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)
+);
+
+CREATE TABLE Preferences (
+    id INTEGER PRIMARY KEY,
+    utilisateur_id INTEGER,
+    genre TEXT,
+    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)
+);
+
+
 
 
