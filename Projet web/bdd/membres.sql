c4f395d844d3331dab15a35470ac79f16bc48412
Rabbras <149598576+Rabbras@users.noreply.github.com>
2024-05-12 14:06:03 +0200

    Add files via upload

--- Projet web/bdd/membres.sql
+++ Projet web/bdd/membres.sql
@@ -17,11 +17,12 @@ CREATE TABLE membres(
     age INT,
     token TEXT,
     pp TEXT,
+    bannier TEXT,
     date_inscription DATE
 );
 
 CREATE TABLE IF NOT EXISTS Amis  (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     utilisateur_id INTEGER,
     ami_id INTEGER,
     FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id),
@@ -29,15 +30,17 @@ CREATE TABLE IF NOT EXISTS Amis  (
 );
 
 CREATE TABLE IF NOT EXISTS Messages   (
-    id INTEGER PRIMARY KEY,
-    utilisateur_id INTEGER,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    utilisateur_id1 INTEGER,
+    utilisateur_id2 INTEGER,
     contenu TEXT,
     date_publication DATETIME,
-    FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)
+    FOREIGN KEY (utilisateur_id1) REFERENCES Utilisateurs(id)
+    FOREIGN KEY (utilisateur_id2) REFERENCES Utilisateurs(id)
 );
 
  CREATE TABLE IF NOT EXISTS Preferences  (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     utilisateur_id INTEGER,
     genre TEXT,
     FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)
@@ -45,14 +48,26 @@ CREATE TABLE IF NOT EXISTS Messages   (
 
 
 CREATE TABLE IF NOT EXISTS Publications   (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     utilisateur_id INTEGER,
     contenu TEXT,
     genre TEXT,
+    like INTEGER,
     date_publication DATETIME,
     FOREIGN KEY (utilisateur_id) REFERENCES Utilisateurs(id)
 );
 
+CREATE TABLE IF NOT EXISTS DemandesAmis (
+    id_demande INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_demandeur INTEGER,
+    id_receveur INTEGER,
+    statut TEXT CHECK(statut IN ('en_attente', 'acceptee', 'rejetee')),
+    date_demande DATETIME,
+    FOREIGN KEY (id_demandeur) REFERENCES Utilisateurs(id),
+    FOREIGN KEY (id_receveur) REFERENCES Utilisateurs(id)
+);
 
 
 
+INSERT INTO Amis (utilisateur_id,ami_id) VALUES (3,1);
+INSERT INTO Amis (utilisateur_id,ami_id) VALUES (1,3);
